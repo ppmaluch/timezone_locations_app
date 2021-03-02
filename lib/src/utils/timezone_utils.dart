271770f9921796813eb54f09f9936780d4509e84
ppmaluch <ppmaluch@gmail.com>
2021-03-02 01:40:54 -0500

    [REFACTOR] moved to utils

--- lib/src/utils/timezone_utils.dart
+++ lib/src/utils/timezone_utils.dart
@@ -2,8 +2,8 @@ import 'dart:convert';
 
 import 'package:flutter/services.dart' show rootBundle;
 
-class TimezoneProvider {
-  TimezoneProvider();
+class TimezoneUtil {
+  TimezoneUtil();
 
   Future<List<String>> loadTimezone() async {
     final response = await rootBundle.loadString('data/timezone_data.json');
@@ -15,4 +15,4 @@ class TimezoneProvider {
   }
 }
 
-final timezoneProvider = new TimezoneProvider();
+final timezoneProvider = new TimezoneUtil();
