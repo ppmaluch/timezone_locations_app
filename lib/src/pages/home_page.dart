5a88e0db7ff314859f09f6e599f17e100e34bb8b
kenriortega <kenriortega@gmail.com>
2021-03-03 23:28:39 -0500

    [ADD] new desing components

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -4,6 +4,7 @@ import 'package:timezone_locations_app/features/world_timezone/domain/model/worl
 import 'package:timezone_locations_app/src/providers/timezone_di_provider.dart';
 import 'package:timezone_locations_app/src/utils/utils.dart';
 import 'package:timezone_locations_app/src/widget/card_widget.dart';
+import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
 
 class HomePage extends StatefulWidget {
   HomePage({Key key}) : super(key: key);
@@ -58,39 +59,40 @@ class _HomePageState extends State<HomePage> {
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
-        Container(
-          decoration: BoxDecoration(
-              color: Colors.white,
-              borderRadius: BorderRadius.vertical(bottom: Radius.circular(30))),
-          padding: EdgeInsets.all(20.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(
-                Environments.appNameTitle,
-                style: TextStyle(
-                  color: Colors.black87,
-                  fontSize: 40.0,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-              SizedBox(
-                height: 5.0,
-              ),
-              Text(
-                'Selected TimeZones - ${timezones.length.toString()}',
-                style: TextStyle(color: Colors.black, fontSize: 25.0),
-              ),
-              SizedBox(
-                height: 20.0,
-              ),
-            ],
-          ),
-        ),
-        SizedBox(
-          height: 20.0,
-        ),
-        _headerArea(),
+        _Header(context: context),
+        // Container(
+        //   decoration: BoxDecoration(
+        //       color: Colors.white,
+        //       borderRadius: BorderRadius.vertical(bottom: Radius.circular(30))),
+        //   padding: EdgeInsets.all(20.0),
+        //   child: Column(
+        //     crossAxisAlignment: CrossAxisAlignment.start,
+        //     children: [
+        //       Text(
+        //         Environments.appNameTitle,
+        //         style: TextStyle(
+        //           color: Colors.black87,
+        //           fontSize: 40.0,
+        //           fontWeight: FontWeight.bold,
+        //         ),
+        //       ),
+        //       SizedBox(
+        //         height: 5.0,
+        //       ),
+        //       Text(
+        //         'Selected TimeZones - ${timezones.length.toString()}',
+        //         style: TextStyle(color: Colors.black, fontSize: 25.0),
+        //       ),
+        //       SizedBox(
+        //         height: 20.0,
+        //       ),
+        //     ],
+        //   ),
+        // ),
+        // SizedBox(
+        //   height: 20.0,
+        // ),
+        // _headerArea(),
         SizedBox(
           height: 15,
         ),
@@ -134,5 +136,185 @@ class _HomePageState extends State<HomePage> {
             child: Padding(
                 padding: const EdgeInsets.all(10.0), child: _cardList()))
         : Container();
+  }
+}
+
+// View one style
+class _Header extends StatelessWidget {
+  const _Header({
+    Key key,
+    @required this.context,
+  }) : super(key: key);
+
+  final BuildContext context;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: MediaQuery.of(context).size.height * 0.20,
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.only(
+          bottomLeft: Radius.circular(40.0),
+          bottomRight: Radius.circular(40.0),
+        ),
+      ),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          SizedBox(
+            height: MediaQuery.of(context).size.height * 0.15,
+            child: CircleAvatar(
+              radius: 55.0,
+              backgroundColor: Colors.white,
+              backgroundImage: AssetImage("assets/images/timeZoneHeader.png"),
+            ),
+          ),
+          SizedBox(
+            width: MediaQuery.of(context).size.width * 0.05,
+          ),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                Environments.appNameTitle,
+                style: TextStyle(
+                    color: kPrimaryBrown,
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold),
+              ),
+              Text(
+                "Selected TimeZones: #12",
+                style: TextStyle(
+                  color: kPrimaryBrown,
+                  fontSize: 12,
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class _ActionsCard extends StatelessWidget {
+  const _ActionsCard({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Padding(
+          padding: const EdgeInsets.symmetric(
+            horizontal: 25.0,
+            vertical: 10.0,
+          ),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Spacer(),
+              Text(
+                "Dark Mode",
+                style: TextStyle(color: Colors.white),
+              ),
+              IconButton(
+                icon: Icon(
+                  Icons.lightbulb,
+                  color: Colors.white,
+                ),
+                onPressed: () {
+                  print("dark mode on");
+                },
+              ),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class _TimeZoneCards extends StatelessWidget {
+  const _TimeZoneCards({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Padding(
+            padding: const EdgeInsets.only(left: 25.0, top: 20, bottom: 12),
+            child: Text(
+              "TimeZones List",
+              style: TextStyle(color: Colors.white),
+            ),
+          ),
+          Expanded(
+            child: StaggeredGridView.countBuilder(
+              crossAxisCount: 4,
+              itemCount: 8,
+              itemBuilder: (BuildContext context, int index) {
+                return Container(
+                  margin: const EdgeInsets.symmetric(
+                    vertical: 10.0,
+                    horizontal: 20,
+                  ),
+                  decoration: BoxDecoration(
+                    color: kPrimaryGrey,
+                    borderRadius: BorderRadius.circular(30.0),
+                  ),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      // SizedBox(
+                      //   height: MediaQuery.of(context).size.height * 0.25,
+                      //   child: CircleAvatar(
+                      //     radius: 25.0,
+                      //     backgroundColor: Colors.white,
+                      //     backgroundImage:
+                      //         AssetImage("assets/images/timeZoneHeader.png"),
+                      //   ),
+                      // ),
+                      Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            "America/Havana",
+                            style: TextStyle(
+                                color: kPrimaryBrown,
+                                fontSize: 12,
+                                fontWeight: FontWeight.bold),
+                          ),
+                          Text(
+                            "12/w",
+                            style: TextStyle(
+                              color: kPrimaryBrown,
+                              fontSize: 12,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                );
+              },
+              staggeredTileBuilder: (int index) {
+                return StaggeredTile.count(2, index.isEven ? 2 : 1);
+              },
+              mainAxisSpacing: 4.0,
+              crossAxisSpacing: 4.0,
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
