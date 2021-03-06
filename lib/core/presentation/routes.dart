51cb6e3fcf0febf69ff568e693770c2f8a679226
ppmaluch <ppmaluch@gmail.com>
2021-03-06 00:11:33 -0500

    [REFACTOR] project structure changed due to FDD architecture

--- lib/core/presentation/routes.dart
+++ lib/core/presentation/routes.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:timezone_locations_app/src/pages/home_page.dart';
+import 'package:timezone_locations_app/features/general/presentation/pages/home_page.dart';
 import 'package:timezone_locations_app/features/world_timezone/pages/search_page.dart';
 import 'package:timezone_locations_app/core/res/utils.dart';
 
